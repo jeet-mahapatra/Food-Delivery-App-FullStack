a86d32a3f7083e5d0df9d407b64c36a400029fdf
jeet-mahapatra <jeetmahapatra2004@gmail.com>
2025-02-26 02:43:47 +0530

    Update API URL in StoreContext to use production endpoint

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext();
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://food-delivary-app-full-stack-api.vercel.app/";
   const [token, setToken] = useState("");
   const [food_list, setFood_list] = useState([]);
 
